ec53d57144e8e1b57abb0252b14ca7e0958bfd68
wuxueqin <wuxueqin@users.noreply.github.com>
2025-05-20 14:04:59 +0800

    Update custom-header.vue

--- docs/examples/table/custom-header.vue
+++ docs/examples/table/custom-header.vue
@@ -46,26 +46,5 @@ const handleDelete = (index: number, row: User) => {
   console.log(index, row)
 }
 
-const tableData: User[] = [
-  {
-    date: '2016-05-03',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-02',
-    name: 'John',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-04',
-    name: 'Morgan',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-01',
-    name: 'Jessy',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-]
+const tableData: User[] = []
 </script>
